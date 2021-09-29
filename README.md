7fbae3390a1074af5b6c53974077dc05c4dda64e
叶芝秋 <zlang0@163.com>
2021-09-29 12:03:16 +0800

    Structural adjustment

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Quickly search a selected paragraph of text in the browser by configuring the se
 
 
 ## Installation
-Before you install this plugin, you need to install my another plugin [dctxmenu](https://github.com/absop/dctxmenu)(**Dynamic Context Menu**). Because this plugin(**SearchOnline**) uses **dctxmenu** to generate menus dynamically.
+This plugin depends on the[dctxmenu](https://github.com/absop/dctxmenu) plugin. Before installing this plugin, you must install `dctxmenu` plugin.
 
 ### !! Note:
 Please make sure that **dtcxmenu** is installed correctly in a subdirectory called **dtcxmenu** in your package directory. If you're using `git clone`, you won't have this problem, but if you're using a browser to download it, you'll need to change the folder name.
---
+++ make_menu.py
@@ -0,0 +1,71 @@
+import webbrowser
+import sublime
+import sublime_plugin
+
+try:
+    from dctxmenu import menu
+except:
+    sublime.error_message(
+        f'The plugin `dctxmenu` is not installed, {__package__} stoped')
+    raise
+
+
+class SerachOnlineMenuCreater(menu.MenuCreater):
+    def context_menu(self, event):
+        if len(self.platforms) > 0:
+            platforms = sorted(self.platforms)
+            if content := self.get_selected(event):
+                if len(platforms) == 1:
+                    platform = platforms[0]
+                    url = self.platforms[platform]
+                    return self.item(
+                        "Search With " + platform,
+                        'open_file_with_default_application',
+                        {"file": url % content }
+                    )
+                else:
+                    return self.folded_item(
+                        self.caption,
+                        [ self.item(
+                            platform,
+                            'open_file_with_default_application',
+                            {"file": url % content}
+                            )
+                         for platform, url in self.platforms.items()
+                        ]
+                    )
+        return None
+
+    def get_selected(self, event):
+        view = self.view
+        pt = view.window_to_text((event["x"], event["y"]))
+        selected = view.sel()
+        if view.has_non_empty_selection_region():
+            selection = selected[0]
+            if not selection.empty() and selection.contains(pt):
+                content = view.substr(selected[0]).strip()
+                if content:
+                    return content
+        return None
+
+    @classmethod
+    def init(cls):
+        cls.caption = settings.get('caption', 'Search Online')
+        cls.platforms = settings.get('platforms', {})
+
+
+def reload_settings():
+    global settings
+    settings = sublime.load_settings('SearchOnline.sublime-settings')
+    settings.add_on_change('caption', SerachOnlineMenuCreater.init)
+    SerachOnlineMenuCreater.init()
+
+
+def plugin_loaded():
+    sublime.set_timeout_async(reload_settings)
+    menu.register(__name__, SerachOnlineMenuCreater)
+
+
+def plugin_unloaded():
+    settings.clear_on_change('caption')
+    menu.remove(__name__)
